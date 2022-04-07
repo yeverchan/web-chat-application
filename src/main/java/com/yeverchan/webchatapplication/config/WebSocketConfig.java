432504f9255bcf0e45839419567bfeb5a1fa3cd0
yeverchan <75665160+yeverchan@users.noreply.github.com>
2022-04-07 21:19:37 +0900

    Test ec2

--- src/main/java/com/yeverchan/webchatapplication/config/WebSocketConfig.java
+++ src/main/java/com/yeverchan/webchatapplication/config/WebSocketConfig.java
@@ -14,8 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat-room")
-//                .setAllowedOrigins("http://localhost:63342")
-                .setAllowedOrigins("/*")
+                .setAllowedOrigins("http://localhost:63342")
                 .withSockJS();
     }
 
